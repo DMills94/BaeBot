e9e2f406194ed033459c1e4f45b6ea0d58c3945a
DMills94 <davidj-mills@live.co.uk>
2018-09-17 15:12:11 +0100

    recent shows if top play

--- commands/recent.js
+++ commands/recent.js
@@ -30,7 +30,7 @@ module.exports = {
         }
         else {
             username = args.join('_');
-        };
+        }
 
         if (!username) {
             return;
@@ -43,48 +43,97 @@ module.exports = {
                 u: username,
                 limit: "1"
             }
-        }).then(resp => {
-            if (resp.data.length == 0) {
-                m.reply("That username does not exist, or has not played in over 24 hours! Please try again.")
-                return;
-            } else {
-                const recent = resp.data[0];
-                //Second Call
-                axios.get("api/get_user", {
-                    params: {
-                        k: osuApiKey,
-                        u: username
-                    }
-                }).then(resp => {
-
-                    let userInfo = resp.data[0];
-
-                    //Third Call
-                    axios.get("api/get_beatmaps", {
+        })
+            .then(resp => {
+                if (resp.data.length == 0) {
+                    m.reply("That username does not exist, or has not played in over 24 hours! Please try again.")
+                    return;
+                }
+                else {
+                    const recent = resp.data[0];
+                    //Second Call
+                    axios.get("api/get_user", {
                         params: {
                             k: osuApiKey,
-                            b: recent.beatmap_id
+                            u: username
                         }
-                    }).then(resp => {
-                        beatmapInfo = resp.data[0];
-
-                        recent.enabled_mods = functions.determineMods(recent);
-
-                        userAcc = "";
-                        recent.accuracy = functions.determineAcc(recent);
-
-                        let playDate = Date.parse(recent.date); //UTC + 0
-                        let currentDate = Date.now() + 25200000; //UTC + 7
-                        recent.date = functions.timeDifference(currentDate, playDate);
-
-                        calculate(beatmapInfo, recent, userInfo, m, rankingEmojis)
                     })
-                })
-            }
-        }).catch(err => {
+                        .then(resp => {
+
+                            let userInfo = resp.data[0];
+
+                            //Third Call
+                            axios.get("api/get_beatmaps", {
+                                params: {
+                                    k: osuApiKey,
+                                    b: recent.beatmap_id
+                                }
+                            })
+                                .then(resp => {
+                                    beatmapInfo = resp.data[0];
+
+                                    axios.get('api/get_user_best', { params: {
+                                            k: osuApiKey,
+                                            u: username,
+                                            limit: 100
+                                        }
+                                    })
+                                        .then(resp => {
+                                            const topPlays = resp.data
+
+                                            for (let play in topPlays) {
+                                                let scoreMatch = true
+
+                                                const aProps = Object.getOwnPropertyNames(recent)
+                                                const bProps = Object.getOwnPropertyNames(topPlays[play])
+
+                                                if (aProps.length !== bProps.length) {
+                                                    scoreMatch = false
+                                                }
+
+                                                for (let prop in aProps) {
+                                                    const propName = aProps[prop]
+
+                                                    if (recent[propName] !== topPlays[play][propName]) {
+                                                        scoreMatch = false
+                                                    }
+                                                }
+
+                                                if (scoreMatch) {
+                                                    recent.playNumber = parseInt(play) + 1
+                                                }
+                                            }
+
+                                            recent.enabled_mods = functions.determineMods(recent);
+
+                                            userAcc = "";
+                                            recent.accuracy = functions.determineAcc(recent);
+
+                                            let playDate = Date.parse(recent.date); //UTC + 0
+                                            let currentDate = Date.now() + 25200000; //UTC + 7
+                                            recent.date = functions.timeDifference(currentDate, playDate);
+
+                                            calculate(beatmapInfo, recent, userInfo, m, rankingEmojis)
+                                        })
+                                        .catch(err => {
+                                        console.log(err)
+                                        m.channel.send("Error! More info: " + err);
+                                        })
+
+
+
+                                })
+                        })
+                        .catch(err => {
+                        console.log(err)
+                        m.channel.send("Error! More info: " + err);
+                        })
+                }
+            })
+            .catch(err => {
             console.log(err)
             m.channel.send("Error! More info: " + err);
-        })
+            })
     }
 };
 
@@ -145,12 +194,32 @@ const generateRecent = (m, userInfo, beatmapInfo, recent, performancePP, maxPP, 
     let embed = new Discord.RichEmbed()
         .setColor("#c0c0c0")
         .setAuthor(`Recent Play for ${userInfo.username}: ${parseFloat(userInfo.pp_raw).toLocaleString('en')}pp (#${parseInt(userInfo.pp_rank).toLocaleString('en')} ${userInfo.country}#${parseInt(userInfo.pp_country_rank).toLocaleString('en')})`, `https://a.ppy.sh/${userInfo.user_id}`, `https://osu.ppy.sh/users/${userInfo.user_id}`)
-        .setURL("https://osu.ppy.sh/b/" + beatmapInfo.beatmap_id)
         .setThumbnail("https://b.ppy.sh/thumb/" + beatmapInfo.beatmapset_id + "l.jpg")
-        .setTitle(beatmapInfo.artist + " - " + beatmapInfo.title + " [" + beatmapInfo.version + "]")
+        .setDescription(`**[${beatmapInfo.artist} - ${beatmapInfo.title} [${beatmapInfo.version}]](https://osu.ppy.sh/b/${beatmapInfo.beatmap_id})**`)
         .addField(`\u2022 \:star: **${stars}*** ${recent.enabled_mods} \n\u2022 ${rankImage} | Score: ${parseInt((recent.score)).toLocaleString("en")} (${recent.accuracy}%) | ${recent.rank === "F_" ? "~~**" + performancePP + "pp**/" + maxPP + "pp~~" : "**" + performancePP + "pp**/" + maxPP + "pp"}`, `\u2022 ${recent.maxcombo === beatmapInfo.max_combo ? "**" + recent.maxcombo + "**" : recent.maxcombo}x/**${beatmapInfo.max_combo}x** {${recent.count300}/${recent.count100}/${recent.count50}/${recent.countmiss}} | ${recent.date}`)
         .setFooter("Message sent: ")
         .setTimestamp()
+
+    if (recent.playNumber) {
+        switch (score.playNumber) {
+            case 1:
+                colour = "#FFD700"
+                break
+            case 2:
+                colour = "#FFFFFF"
+                break
+            case 3:
+                colour = "#cd7f32"
+                break
+            default:
+                colour = "#c0c0c0"
+                break
+        }
+
+        embed
+            .setTitle(`__PERSONAL BEST #${recent.playNumber}__`)
+            .setColor(colour)
+    }
 
     //Send Embed to Channel
     m.channel.send({embed: embed});
